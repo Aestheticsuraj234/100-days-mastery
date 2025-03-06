2a0d46a369f2cbc09c18bd23655f53907750e1c3
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-03-06 22:52:25 +0530

    js-day-1

---
+++ frontend/javascript/day-13/main.js
@@ -0,0 +1,28 @@
+console.log("Hello from main.js");
+
+
+// keyword variablename = value;
+
+// var age =20;
+// console.log(age)
+
+
+// let name = "Suraj";
+// console.log(name);
+
+
+// const pi = 3.14;
+// console.log(pi);
+
+
+let array = [1,"2",true,4,5];
+console.log(array);
+
+let obj = {
+    name:"suraj",
+    age:20
+}
+
+console.log(obj);
+
+// function add
