3cdc9c5489c841b7d1f100e6330a0ab8bc72fe2d
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-03-08 10:33:44 +0530

    day-15

---
+++ frontend/javascript/day-15/main.js
@@ -0,0 +1,102 @@
+// let age = 21;
+
+// *if-else
+
+// if (age === 18) {
+//     console.log("Yes you can vote");
+// }
+// else {
+//     console.log("No you can't vote");
+// }
+
+// if(age >=50){
+//     console.log("The age is 50 or more")
+// }
+// else if(age === 38){
+//     console.log("The age is 38")
+// }
+// else if (age <= 20){
+//     console.log("The age is less than 20")
+// }
+
+// else{
+//     console.log("The age is less than 50")
+// }
+
+// let marks = 80;
+
+// if (marks >= 90) {
+
+//   console.log("Grade: A");
+
+// } else if (marks >= 75) {
+
+//   console.log("Grade: B");
+
+// } else if (marks >= 50) {
+
+//   console.log("Grade: C");
+
+// } else {
+
+//   console.log("Grade: F (Fail)");
+
+// }
+
+// Nested if else
+
+// let fruit = "apple";
+
+// if (fruit === "apple") {
+//   let isHealthy = false;
+
+//   if (isHealthy) {
+//     console.log("The fruit is healthy");
+//   } else {
+//     console.log("The fruit is not healthy");
+//   }
+
+// } else {
+//   console.log("The fruit is not apple");
+// }
+
+// *Switch case
+
+// switch(age){
+//     case 1:
+
+//     case 2:
+
+//     case 3:
+
+//     default:
+
+// }
+
+let day = 8;
+
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  case 7:
+    console.log("Sunday");
+    break;
+  default:
+    console.log("Invalid day number");
+}
