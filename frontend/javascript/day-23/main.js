a95dc04e71a68ae8a33705ba0ebab78370c71231
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-03-21 12:49:29 +0530

    day-23

---
+++ frontend/javascript/day-23/main.js
@@ -0,0 +1,36 @@
+
+
+
+// function mySync(){
+//     console.log("Hello")
+//     console.log("Hola")
+//     console.log("Hey")
+// }
+
+// mySync()
+
+
+// function myAsync(){
+//     console.log("Hello")
+
+//     setTimeout(()=>{
+//         console.log("I am from setTimeout")
+//     } , 3000)
+
+//     console.log("Hola")
+// }
+
+
+myAsync();
+
+
+// setTimeout;
+// setInterval;
+// clearInterval;
+// clearTimeout;
+
+// fetch;
+// Promise;
+
+// .then and .catch
+// async and await
