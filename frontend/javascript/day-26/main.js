fc5e32604afd6d2dd6224b8bbcb6fb7050e73f96
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-03-22 10:42:37 +0530

    day-26

---
+++ frontend/javascript/day-26/main.js
@@ -0,0 +1,34 @@
+
+//* 1. Localstorage in JavaScript
+
+// localStorage.setItem("username" , "suraj");
+// localStorage.setItem("username","karan")
+
+// let userdata = [
+//     {
+//         name: "suraj",
+//     },
+//     {
+//         name: "karan",
+//     },
+//     {
+//         name: "rahul",
+//     }
+// ]
+
+// console.log(typeof userdata);
+
+// const user = JSON.stringify(userdata);
+
+// localStorage.setItem("username" , user)
+
+
+// const data = localStorage.getItem("username")
+
+// console.log(data);
+
+// localStorage.removeItem("username")
+
+// localStorage.clear()
+
+// sessionStorage.setItem("fruit" , "🍇");
